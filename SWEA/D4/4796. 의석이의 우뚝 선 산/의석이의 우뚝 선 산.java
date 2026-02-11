378f692e3cfcda763591d5b01fcd444dbb256910
kiddo-psh <kiddo.psh@gmail.com>
2026-02-11 12:25:48 +0900

    [D4] Title: 의석이의 우뚝 선 산, Time: 667 ms, Memory: 101,696 KB -BaekjoonHub

--- SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
+++ SWEA/D4/4796. 의석이의 우뚝 선 산/의석이의 우뚝 선 산.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
 
+/*
+ * 
+ * 메모리: 101,548 kb
+ * 실행시간: 646 ms
+ * 
+ * 
+ */
+
 public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		for (int tc=1; tc<=T; tc++) {
 			int N = sc.nextInt();
-			int[] mountains = new int[N];
-			for (int i=0; i<N; i++) {
-				mountains[i] = sc.nextInt();
-			}
-			
+			int l = sc.nextInt();
+			int r;
+
 			int left = 0; int right = 0;
 			int result = 0;
-			
 			for (int i=0; i<N-1; i++) {
-				if (mountains[i] < mountains[i+1]) {
+				r = sc.nextInt();
+				if (l < r) {
 					if (right != 0) {
 						result += left*right;
 						left = 0; right = 0;
 					} 
 					left++;
-				} 
-				else {
+				} else {
 					right++;
 				}
+				l = r;
 			}
 			
 			result += left*right;
