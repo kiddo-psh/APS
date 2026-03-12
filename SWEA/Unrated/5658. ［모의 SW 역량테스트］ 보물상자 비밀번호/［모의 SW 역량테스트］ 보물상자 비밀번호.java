e19ed62a069d8d617da341a07a1295867af0d24b
kiddo-psh <kiddo.psh@gmail.com>
2026-03-12 12:25:41 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보물상자 비밀번호, Time: 89 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
+++ SWEA/Unrated/5658. ［모의 SW 역량테스트］ 보물상자 비밀번호/［모의 SW 역량테스트］ 보물상자 비밀번호.java
@@ -1,57 +1,59 @@
-import java.io.*;
-import java.util.*;
-
-/*
- * 
- * 메모리: 26,368 kb / 실행시간: 89 ms
- * 
- * */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class Solution {
 	static int N, K;
-	static int[] nums = new int[28]; // 만들 수 있는 숫자 넣을 배열
-	static int numsIndex = 0; // 배열 인덱스
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringBuilder output = new StringBuilder();
-		StringTokenizer st;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
-		
 		for (int tc=1; tc<=T; tc++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			
-			StringBuilder input = new StringBuilder(br.readLine());
-			input.append(input.substring(0, N/4));
+			int len = N/4;
+			char[] nums = new char[N+N/4];
+			TreeSet<Integer> numSet = new TreeSet<>();
 			
-			// 배열 초기화
-			Arrays.fill(nums, 0);
-			numsIndex = 0;
-			
+			String S = br.readLine();
 			for (int i=0; i<N; i++) {
-				int num = 0;
-				for (int j=i; j<i+N/4; j++) {
-					int n = Character.isDigit(input.charAt(j)) ? input.charAt(j)-'0' : input.charAt(j)-'A'+10;
-					num = num*16 + n;
+				nums[i] = S.charAt(i);
+			}
+			for (int i=N; i<N+N/4; i++) {
+				nums[i] = nums[i-N];
+			}
+			
+			StringBuilder sb = new StringBuilder();
+			for (int i=0; i<len; i++) {
+				for (int j=i; j<i+N; j++) {
+					 if (sb.length()==len) {
+						 int x = Integer.parseInt(sb.toString(), 16);
+						 numSet.add(x);
+						 sb.delete(0, sb.length());
+					 }
+					 sb.append(nums[j]);
 				}
-				nums[numsIndex++] = num;
+			}
+			int x = Integer.parseInt(sb.toString(), 16);
+			numSet.add(x);
+			
+			Iterator<Integer> it = numSet.descendingIterator();
+			int val = -1;
+			
+			for (int i=0; i<K; i++) {
+				val = it.next();
 			}
 			
-			int result = 0;
-			Arrays.sort(nums);
-			
-			int order = 0;
-			for (int i=27; i>=0; i--) {
-				if (i!=27 && nums[i]==nums[i+1]) continue; // 중복제거
-				order++;
-				if (order==K) result = nums[i];
-			}
-			
-			output.append("#").append(tc).append(" ")
-			.append(result).append("\n");
+			output.append("#").append(tc).append(" ").append(val).append("\n");
 		}
 		System.out.println(output);
+		br.close();
 	}
 }
