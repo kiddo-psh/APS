e4660931d8db43fc1f81b09960706d447c56f42e
kiddo-psh <kiddo.psh@gmail.com>
2026-04-08 09:03:12 +0900

    [Silver III] Title: 1로 만들기, Time: 200 ms, Memory: 21664 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int[] dp = new int[x+1];
+
+        Arrays.fill(dp, 1_000_000);
+        dp[x] = 0;
+
+        for (int i=x; i>0; i--) {
+            if (i%3==0) dp[i/3] = Math.min(dp[i/3], dp[i] + 1);
+            if (i%2==0) dp[i/2] = Math.min(dp[i/2], dp[i] + 1);
+            dp[i-1] = Math.min(dp[i-1], dp[i]+1);
+        }
+
+        System.out.println(dp[1]);
+    }
+}
