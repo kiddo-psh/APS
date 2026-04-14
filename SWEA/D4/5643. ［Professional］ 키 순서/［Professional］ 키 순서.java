b9f9f30212121b4e76d836f5e31484f3c0368f36
kiddo-psh <kiddo.psh@gmail.com>
2026-04-14 14:23:32 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,290 ms, Memory: 101,700 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -5,6 +5,7 @@ import java.util.*;
 
 public class Solution {
     static int N, M;
+    static final int INF = 1_000_000_000;
     public static void main(String[] args) throws IOException {
         StringBuilder output = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,57 +14,40 @@ public class Solution {
             N = Integer.parseInt(br.readLine());
             M = Integer.parseInt(br.readLine());
 
-            List<Integer>[] graph = new ArrayList[N+1];
-            for (int i=1; i<=N; i++) graph[i] = new ArrayList<>();
-
-            List<Integer>[] reverse = new ArrayList[N+1];
-            for (int i=1; i<=N; i++) reverse[i] = new ArrayList<>();
+            int[][] graph = new int[N+1][N+1];
+            for (int i=1; i<=N; i++) Arrays.fill(graph[i], INF);
+            for (int i=1; i<=N; i++) graph[i][i] = 0;
 
             while (M-->0) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
 
-                graph[a].add(b);
-                reverse[b].add(a);
+                graph[a][b] = 1;
             }
 
-            //int[] inCount = new int[N+1]; // 나보다 작은 사람 수
-            //int[] outCount = new int[N+1]; //나보다 큰 사람 수
+            for (int k=1; k<=N; k++) {
+                for (int i=1; i<=N; i++) {
+                    for (int j=1; j<=N; j++) {
+                        graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                    }
+                }
+            }
 
             int answer = 0;
-            for (int i=1; i<=N; i++) {
-                int outCount = bfs(i, graph);
-                int inCount = bfs(i, reverse);
 
-                if (outCount + inCount == N-1) answer++;
+            for (int i=1; i<=N; i++) {
+                int cnt = 0;
+                for (int j=1; j<=N; j++) {
+                    if (i==j) continue;
+                    if (graph[i][j] != INF || graph[j][i] != INF) cnt++;
+                }
+                if (cnt == N-1) answer++;
             }
 
             output.append("#").append(tc).append(" ").append(answer).append("\n");
         }
         System.out.print(output);
         br.close();
-    }
-
-    private static int bfs(int i, List<Integer>[] graph) {
-        int count = 0;
-
-        boolean[] visited = new boolean[N+1];
-        visited[i] = true;
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(i);
-
-        while (!q.isEmpty()) {
-            int u = q.poll();
-
-            for (int v : graph[u]) {
-                if (visited[v]) continue;
-                visited[v] = true;
-                q.offer(v);
-                count++;
-            }
-        }
-
-        return count;
     }
 }
