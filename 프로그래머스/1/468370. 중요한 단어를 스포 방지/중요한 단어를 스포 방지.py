af724bebf3e525660d90b3ef9fcf499260de2da5
kiddo-psh <kiddo.psh@gmail.com>
2026-06-02 10:59:18 +0900

    [level 1] Title: 중요한 단어를 스포 방지, Time: 5.23 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
+++ 프로그래머스/1/468370. 중요한 단어를 스포 방지/중요한 단어를 스포 방지.py
@@ -5,15 +5,18 @@ def solution(message, spoiler_ranges):
     
     idx = 0
     
+    if "  " in message:
+        raise RuntimeError("연속된 공백 발견")
+    
     for word in message.split(" "):
         start = idx
         end = start + len(word) - 1
-        idx += len(word) + 1
+        idx = end + 2
         
         is_spoiler = any(
             start <= right and end >= left
             for left, right in spoiler_ranges
-        )
+    )
                 
         if is_spoiler:
             spoiler_words.add(word)
